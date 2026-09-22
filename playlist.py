c9ad488a8b74f6e0e1e485efd8146ec836e8b748
isaachiggins-web <isaac.higgins@ascte.org>
2026-09-22 09:15:21 -0500

    Song added
    I added a singuler song by Marilyin Manson and stuff

--- playlist.py
+++ playlist.py
@@ -4,6 +4,7 @@ SONGS = [
     ("Imagine", "John Lennon"),
     ("Blinding Lights", "The Weeknd"),
     ("Viva La Vida", "Coldplay"),
+    ("Sweet Dreams", "Marilyn Manson")
 ]
 
 def print_playlist(songs):
